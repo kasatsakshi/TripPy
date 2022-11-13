08a67952a3e06bcfaf03f1c3a0a9a9afb12c3aba
Ratika Bhuwalka <ratika.bhuwalka@sjsu.edu>
2022-11-12 20:00:13 -0800

    script update

--- dataPipeline/dataExtraction/geoapify/script.py
+++ dataPipeline/dataExtraction/geoapify/script.py
@@ -15,20 +15,25 @@ def form_url(row):
 df['URL'] = df.apply (lambda row: form_url(row), axis=1)
 top_cities = ["New York", "Los Angeles", "Las Vegas", "Orlando", "Miami", "San Francisco"]
 df_top = df[df['CITY'].isin(top_cities)]
+df_top = df_top.reset_index()  
 
 pat = ""
 authorization = str(base64.b64encode(bytes(':'+pat, 'ascii')), 'ascii')
-
-
-
-url="https://api.geoapify.com/v2/places?categories=tourism,entertainment,catering.restaurant,beach&filter=circle:-73.996705,40.74838,1000&bias=proximity:-73.996705,40.74838&limit=500&apiKey=c8a825d7da834647b40102fcad6d5b3f"
-
 headers = {
  'Accept': 'application/json',
  'Authorization': 'Basic '+authorization
 }
 
-response = requests.get(url, headers=headers)
+for index, row in df_top.iterrows():
+    
+    city = row["CITY"]
+    state = row["STATE_CODE"]
+    county = row["COUNTY"]
+    url = row["URL"]
+    
+    filename = city+'-'+state+'.json'
+    
+    response = requests.get(url, headers=headers)
 
-with open("data.json", "wb") as text_file:
-    text_file.write(response.content)
+    with open(filename, "wb") as text_file:
+        text_file.write(response.content)

22032ac565fab07e88030297ef10586ad738db7b
Ratika Bhuwalka <ratika.bhuwalka@sjsu.edu>
2023-04-04 15:51:22 -0700

    models

--- backend/src/models/groupModel.js
+++ backend/src/models/groupModel.js
@@ -4,22 +4,16 @@ const Schema = mongoose.Schema;
 
 const groupSchema = new Schema({
     groupName: { type: String},
-
     members: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "user",
       }],
-
     bookMarkeditineraries: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "itinerary",
       }],
-
-    admin : { type: mongoose.Schema.Types.ObjectId,
+    createdBy : { type: mongoose.Schema.Types.ObjectId,
     ref: "user"},
-    
-    
-
 });
 
 const groupModel = mongoose.model("group",groupSchema);
--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -7,6 +7,8 @@ const userSchema = new Schema({
 	username: { type: String },
     password: { type : String },
     email: { type: String },
+    bookmarkedItineraries: {type: mongoose.Schema.Types.ObjectId,
+                    ref: "itinerary"}
 });
 
 const userModel = mongoose.model("user",userSchema);

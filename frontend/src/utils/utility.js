1400e04e2fef0d9c6182ff1c897f353aad78dfe2
aygupta9800 <ayushgupta9800@gmail.com>
2023-05-10 13:05:00 -0700

    email format check on signup

---
+++ frontend/src/utils/utility.js
@@ -0,0 +1,5 @@
+export const isValidEmail = (email) => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; //eslint-disable-line
+    // console.log('Email validity', re.test(String(email).toLowerCase()));
+    return re.test(String(email).toLowerCase());
+}
